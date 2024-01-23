0715d041c8efb07a505bf1fb35d3aa5c75d60cfd
AnnaZabolotnia <137837327+AnnaZabolotnia@users.noreply.github.com>
2024-01-23 17:05:10 +0200

    goit-js-hw-09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,20 @@
 const form = document.querySelector('form');
-
 const emailField = document.querySelector('input');
 const messageField = document.querySelector('textarea');
 
 let toSave = localStorage.getItem('feedback-form-state');
 if (toSave) {
   toSave = JSON.parse(toSave);
-  emailField.value = toSave.email;
-  messageField.value = toSave.message;
+  if (toSave.email && toSave.message) {
+    emailField.value = toSave.email;
+    messageField.value = toSave.message;
+  } else {
+    localStorage.removeItem('feedback-form-state');
+    toSave = {
+      email: '',
+      message: '',
+    };
+  }
 } else {
   toSave = {
     email: '',
@@ -17,7 +24,7 @@ if (toSave) {
 
 form.addEventListener('input', e => {
   const name = e.target.name;
-  const value = e.target.value;
+  const value = e.target.value.trim();
 
   if (name === 'email') {
     toSave.email = value;
@@ -30,12 +37,17 @@ form.addEventListener('input', e => {
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  console.log('submit', toSave);
-  emailField.value = '';
-  messageField.value = '';
-  toSave = {
-    email: '',
-    message: '',
-  };
-  localStorage.removeItem('feedback-form-state');
+
+  if (toSave.email && toSave.message) {
+    console.log('submit', toSave);
+    emailField.value = '';
+    messageField.value = '';
+    toSave = {
+      email: '',
+      message: '',
+    };
+    localStorage.removeItem('feedback-form-state');
+  } else {
+    console.log('Error');
+  }
 });
